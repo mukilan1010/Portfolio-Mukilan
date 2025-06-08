90a8b3e11fa50500b613d16d89ec88470006d06b
mukilan1010 <146624884+mukilan1010@users.noreply.github.com>
2025-06-08 06:02:25 +0530

    FINA DONE

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate=useNavigate();
   return (
     <nav className="bg-black text-white py-4 px-6 shadow-md flex justify-between items-center">
-      <h1 className="text-2xl font-bold">My Portfolio</h1>
+   
+       <Link to="/" className="text-2xl font-bold">Mukilan N</Link>
+
+     
       <ul className="flex space-x-6">
         <li>
           <Link to="/" className="hover:text-gray-400 transition-colors">Home</Link>
